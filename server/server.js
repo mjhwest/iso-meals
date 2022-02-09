3e5582ee9f726e1c6349d1d0778d67b77e313301
mjhwest89 <mjhwest89@gmail.com>
2022-02-10 00:42:20 +1030

    go over server and is all good

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.get('*', (req, res) => {
 
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
+    console.log(`API server running on port http://localhost:${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
