af150b953492cb8cc14192f57ec0bf194e4cfdea
mjhwest89 <mjhwest89@gmail.com>
2022-02-10 00:38:31 +1030

    added cateogry model

---
+++ server/models/Category.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const categorySchema = new Schema({
+    name: {
+      type: String,
+      required: true,
+      trim: true, 
+      minlength: 3
+    }
+  });
+
+
+const Category = mongoose.model('Category', categorySchema);
+
+module.exports = Category;
