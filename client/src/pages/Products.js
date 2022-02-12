2c9a8d06b21f29c16167ca55908f0dc210d8e572
mjhwest89 <mjhwest89@gmail.com>
2022-02-12 13:12:06 +1030

    can get product names to render on screen

--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -8,22 +8,24 @@ const Products = () => {
 
   return (
     <>
-    <div className="product-title flex-row justify-center">
-    <h1> Food Bundles  </h1>
-    </div>
 
-  
-     
-    {/* <h1> Food Bundles  </h1>
-    <Row> 
-        {products.map( product => (
-          <Col sm={12} md={6} lg={4} xl={3}>
-              <h3> {product.name} </h3>
-          </Col>
-        ))}
+    <h1> Latest Products</h1>
+    <Row>
+      {/* TRYING TO LOOP THROUGH PRODUCTS.js using.map to make a list  */}
+      {products.map(product => (
+        <Col sm={12} md={6} lg={4} ex={3}>
+      <h3>{product.name} </h3>
+      </Col>
+      ))}
 
     </Row>
-     */}
+    {/* <Row>
+      {products.map(products => (
+        // <Col sm={12} md={6} lg={4} xl={3}>
+          <h3> {products.name}</h3>
+        </Col>
+      ))}
+    </Row> */}
     </>
 
   )
