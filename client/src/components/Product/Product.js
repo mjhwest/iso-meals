0997d26040319d5310c17a1181923926212a53ea
mjhwest89 <mjhwest89@gmail.com>
2022-02-12 23:53:45 +1030

    remove a href and replace with Link to

--- client/src/components/Product/Product.js
+++ client/src/components/Product/Product.js
@@ -2,23 +2,24 @@ import React from 'react'
 import { Card } from 'react-bootstrap'
 import Rating from '../Rating/Rating'
 import './Product.css';
+import { Link } from 'react-router-dom'
 
 const Product = ({product}) => {
   return (
     <Card className='my-3 p-3 rounded' >
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
             <Card.Img src={product.image} variant='top' />
-        </a>
+        </Link>
 
 
         <Card.Body>
-            <a href={`/product/${product._id}`}>
+            <Link to={`/product/${product._id}`}>
                 <Card.Title as='div'>
                     <div className='card-body-title'> 
                     <strong >{product.name}</strong>
                     </div>
                 </Card.Title>
-            </a>
+            </Link>
 
             <Card.Text as='div'> 
             <Rating 
