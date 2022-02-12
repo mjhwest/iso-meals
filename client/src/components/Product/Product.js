5ae6377d5cc85b4558a025e01034db1697731f5b
mjhwest89 <mjhwest89@gmail.com>
2022-02-12 14:22:13 +1030

    changes to product

--- client/src/components/Product/Product.js
+++ client/src/components/Product/Product.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from '../Rating/Rating'
 
 const Product = ({product}) => {
   return (
@@ -17,9 +18,12 @@ const Product = ({product}) => {
             </a>
 
             <Card.Text as='div'> 
-            <div className='my-3'>
+            <Rating 
+            value={product.rating} 
+            text={`${product.numReviews} reveiws`} /> 
+            {/* <div className='my-3'>
                 {product.rating} from {product.numReviews} reveiws
-                </div>
+                </div> */}
                 </Card.Text>
 
             <Card.Text as="h3"> ${product.price}</Card.Text>
