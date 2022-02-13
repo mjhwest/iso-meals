cb2dcf2396e58d7c9ecbe6f930b4ab70a1dd458d
mjhwest89 <mjhwest89@gmail.com>
2022-02-14 01:00:18 +1030

    end of day save

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,79 +1,83 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
-
-const reviewSchema = new Schema ({
-  name: {type: String, required: true}, 
-  //this will be individual review rating, rating further down is avg of all reviews. 
-  rating: {type: Number, required: true}, 
-  comment: {type: String, required: true},  
-}, {
-  timestamps: true, 
-})
-
-const productSchema = new Schema({
-// user field added i want to know which user (admin) created whihc product. 
-  user: {
-    type: mongoose.Schema.Types.ObjectId, 
-    required: true, 
-    ref: 'User'
+const reviewSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    //this will be individual review rating, rating further down is avg of all reviews.
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
   },
-
-  name: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  description: {
-    type: String,
-    required: true, 
-  },
-  image: {
-    type: String
-  },
-  price: {
-    type: Number,
-    required: true,
-    default: 0
-  },
-  // quantity: {
-  //   type: Number,
-  //   min: 0,
-  //   default: 0
-  // },
-
-//added Reviews 
-//reviews will be an array as a product can have many reviews. 
-
-  reviews: [reviewSchema], 
-
-  rating: {
-    type: Number,
-    required: true, 
-    default: 0
-  },
-  numReviews: {
-    type: Number,
-    required: true,
-    default: 0
-  },
-  countInStock: {
-    type: Number,
-    required: true, 
-    default: 0
+  {
+    timestamps: true,
   }
-  // category: {
-  //   type: Schema.Types.ObjectId,
-  //   ref: 'Category',
-  //   required: true
-  // }
-}, 
-{
-  timestamps: true, 
-}
 );
 
-const Product = mongoose.model('Product', productSchema);
+const productSchema = new Schema(
+  {
+    // user field added i want to know which user (admin) created whihc product.
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    // quantity: {
+    //   type: Number,
+    //   min: 0,
+    //   default: 0
+    // },
+
+    //added Reviews
+    //reviews will be an array as a product can have many reviews.
+
+    reviews: [reviewSchema],
+
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    // category: {
+    //   type: Schema.Types.ObjectId,
+    //   ref: 'Category',
+    //   required: true
+    // }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
 
 module.exports = Product;
