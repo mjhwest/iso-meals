03e1eb05b3427ca95c54aeff802838eb20a3f954
mjhwest89 <mjhwest89@gmail.com>
2022-02-14 14:17:26 +1030

    added users

---
+++ server/data/users.js
@@ -0,0 +1,31 @@
+//create array of users 
+//make 1 admin 
+
+const users = [
+
+    {
+        username: 'Admin User',
+        address: '1 Street',
+        phone: "123456789",
+        email: 'admin@email.com', 
+        password: 'xxxxxx', 
+        isAdmin: true
+    }, 
+    {
+        username: 'Sally',
+        address: '1 Sally Street',
+        phone: "2468",
+        email: 'sally@email.com', 
+        password: 'xxxxxx', 
+    },
+    {
+        username: 'Tom',
+        address: '1 Tom Street',
+        phone: "13579",
+        email: 'tom@email.com', 
+        password: 'xxxxxx',
+    },
+
+]
+
+module.exports = users
