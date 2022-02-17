4b0da7eb290d467526ffc5421889d7b255c8195c
mjhwest89 <mjhwest89@gmail.com>
2022-02-17 14:26:38 +1030

    changes

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -8,7 +8,8 @@ import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import { ADD_TO_CART } from "../utils/mutations";
 import { QUERY_CART } from "../utils/queries";
 //need a query to load cart details
-import "./cart.css";
+import "./cart.css"
+
 
 const Cart = () => {
   const { products } = useParams();
@@ -17,46 +18,77 @@ const Cart = () => {
   const { loading, data } = useQuery(QUERY_CART);
   const cartData = data?.user?.cart;
 
-  console.log(cartData);
+  console.log(cartData)
 
   return (
+
+
     <>
-      <Link className="btn btn-dark my-3" to="/products">
-        Go Back
-      </Link>
+<Link className="btn btn-dark my-3" to="/products">
+  Go Back
+</Link>
 
-      <div className="cart-title"> Your Cart Includes</div>
+<div className="cart-title"> Your Cart Includes</div>
 
-      <Row>
-        {loading ? (
-          <div> Loading...</div>
-        ) : (
-          cartData.map((cartItem) => (
-            <Col
-              key={cartItem._id}
-              // sm={12} md={6} lg={4} ex={3}
-            >
-              {/* <Cart  /> We're already in the Cart component. If you try to render a component inside itself it will cause an infinite loop */}
-              <div className="cart-list-items my3 p-3 rounded">
-                <div className="product-title"> {cartItem.name}: </div>
-                <Row className="product-details">
-                  <div className="product-description col text-start">
-                    {cartItem.description}
-                  </div>
-                  <div className="product-price col text-end">
-                    ${cartItem.price}
-                  </div>
-                </Row>
-              </div>
-            </Col>
-          ))
-        )}
+<Row>
+  {loading ? (
+    <div> Loading...</div>
+  ) : (
+    cartData.map((cartItem) => (
+      <Col
+        key={cartItem._id}
+        sm={12} md={12} lg={12} ex={12}
+      >
+        {/* <Cart  /> We're already in the Cart component. If you try to render a component inside itself it will cause an infinite loop */}
+        <div className="cart-list-items my3 p-3 rounded">
+          <div className="product-title"> {cartItem.name}: </div>
+          <Col className="product-details">
+            <div className="product-description col text-start">
+              {cartItem.description}
+            </div>
+            <div className="product-price col text-end">
+              ${cartItem.price}
+            </div>
+          </Col>
+        </div>
+      </Col>
+    )
+    )
+  )}
 
-        <Link className="btn btn-dark my-3" to="/products">
-          Go To Checkout
-        </Link>
-      </Row>
-    </>
+  <Link className="btn btn-dark my-3" to="/products">
+    Go To Checkout
+  </Link>
+</Row>
+</>
+
+    // <>
+    // <Link className="btn btn-dark my-3" to="/products">
+    //     Go Back
+    //   </Link>
+    //   <div className="cart-title"> Your Cart Includes</div>
+    // <Row>
+    //   {
+    //       loading ? (
+    //           <div> Loading...</div>
+    //       ) : (
+    //           cartData.map((cartItem) => (
+    //               <Col key={cartItem._id} sm={12} md={6} lg={4} ex={3} >
+    //                   {/* <Cart  /> We're already in the Cart component. If you try to render a component inside itself it will cause an infinite loop */} 
+    //                  <div className="cart-list-items my3 p-3 rounded"> 
+    //                   <h1>
+    //                   {cartItem.name}, 
+    //                   </h1>
+    //                   {cartItem.description}
+    //                   ${cartItem.price}
+    //                  </div>
+                       
+    //               </Col>
+    //           ))
+    //       )
+    //   }
+    // </Row>
+    // </>
   );
 };
 
