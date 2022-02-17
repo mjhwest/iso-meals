8dafcc7c72181ae27e492b271979fbc60b90f816
mjhwest89 <mjhwest89@gmail.com>
2022-02-17 10:56:44 +1030

    very basic cart css done

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -6,9 +6,9 @@ import { useMutation } from "@apollo/client";
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import { ADD_TO_CART } from "../utils/mutations";
-
 import { QUERY_CART } from "../utils/queries";
 //need a query to load cart details
+import "./cart.css";
 
 const Cart = () => {
   const { products } = useParams();
@@ -17,39 +17,40 @@ const Cart = () => {
   const { loading, data } = useQuery(QUERY_CART);
   const cartData = data?.user?.cart;
 
-  console.log(cartData)
+  console.log(cartData);
 
   return (
-
     <>
-
-    <Link className="btn btn-dark my-3" to="/products">
+      <Link className="btn btn-dark my-3" to="/products">
         Go Back
-      </Link> 
-      <h2> Your Cart Includes</h2>
+      </Link>
 
-    <Row>
-      {
-          loading ? (
-              <div> Loading...</div>
-          ) : (
-              cartData.map((cartItem) => (
-                  <Col key={cartItem._id} sm={12} md={6} lg={4} ex={3} >
-                      {/* <Cart  /> We're already in the Cart component. If you try to render a component inside itself it will cause an infinite loop */} 
-                     <div className="my3 p-3 rounded"> 
-                      <h1>{cartItem.name}, 
-                      ${cartItem.price}</h1>
-                     </div>
-                       
-                  </Col>
-              ))
-          )
-      }
-    </Row>
+      <div className="cart-title"> Your Cart Includes</div>
 
+      <Row>
+        {loading ? (
+          <div> Loading...</div>
+        ) : (
+          cartData.map((cartItem) => (
+            <Col
+              key={cartItem._id}
+              // sm={12} md={6} lg={4} ex={3}
+            >
+              {/* <Cart  /> We're already in the Cart component. If you try to render a component inside itself it will cause an infinite loop */}
+              <div className="cart-list-items my3 p-3 rounded">
+                <div className="product-title"> {cartItem.name}: </div>
+                <row className="product-details">
+                  {cartItem.description} ${cartItem.price}
+                </row>
+              </div>
+            </Col>
+          ))
+        )}
 
-
-      
+        <Link className="btn btn-dark my-3" to="/products">
+          Go To Checkout
+        </Link>
+      </Row>
     </>
   );
 };
