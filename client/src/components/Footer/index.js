e60333d06d557f9b6cf614b1768361546f3151cc
mjhwest89 <mjhwest89@gmail.com>
2022-02-20 18:22:33 +1030

    push socials

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -23,6 +23,37 @@ const Footer = () => {
       <Container>
         <Row>
           <Col className="text-center py-3">Copyright &copy; Iso-Meals</Col>
+          <div className="col-lg-5 col-md-5 col-sm-6 align-items-center"> 
+          <div className="d-flex justify-content-center"> 
+          
+          <FacebookShareButton
+                url={"https://iso-meals.herokuapp.com/"}
+                quote= {" Meals made easy during isolation, check it out! "}>
+                    <FacebookIcon className="mx-3" size={36}/>
+                </FacebookShareButton>
+                <TwitterShareButton
+                url={"https://iso-meals.herokuapp.com/"}
+                quote= {"Meals made easy during isolation, check it out!"}>
+                    <TwitterIcon className="mx-3" size={36}/>
+                </TwitterShareButton>
+                <RedditShareButton
+                url={"https://iso-meals.herokuapp.com/"}
+                quote= {"Meals made easy during isolation, check it out!"}>
+                    <RedditIcon className="mx-3" size={36}/>
+                </RedditShareButton>
+                <LinkedinShareButton
+                url={"https://iso-meals.herokuapp.com/"}
+                quote= {"Meals made easy during isolation, check it out!"}>
+                    <LinkedinIcon className="mx-3" size={36}/>
+                </LinkedinShareButton>
+                <WhatsappShareButton
+                url={"https://iso-meals.herokuapp.com/"}
+                quote= {"Meals made easy during isolation, check it out!"}>
+                    <WhatsappIcon className="mx-3" size={36}/>
+                </WhatsappShareButton>
+          </div> 
+          
+          </div>
         </Row>
       </Container>
     </footer>
