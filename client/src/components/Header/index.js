fec5ad43c7b2c9a661282fbeb898c8fa9cdcefe3
mjhwest89 <mjhwest89@gmail.com>
2022-02-21 16:36:43 +1030

    added contact to nav bar for mobile display

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -109,6 +109,11 @@ const Header = () => {
             */}
                   Products
                 </Link>
+                <Link className="btn btn-lg btn-info m-2" to="/contact">
+              <i className="fa-solid fa-message-middle"></i>
+               {/* this is not working */}
+                Contact
+              </Link>
                 <Link
                   className="btn btn-lg btn-light m-2"
                   to="/cart"
