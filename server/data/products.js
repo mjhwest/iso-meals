081e34a504f6fe3e66791b5bc8542c3d0135687c
mjhwest89 <mjhwest89@gmail.com>
2022-02-21 21:12:01 +1030

    more seed data

--- server/data/products.js
+++ server/data/products.js
@@ -1,86 +1,150 @@
 const products = [
-    {
-        name: "Vegan Bundle", 
-        description: " Vegan breakfast, lunch and dinner for 7 days. ",
-        image: '/images/vegan.jpg',
-        price: 150,
-        rating: 4.5,
-        numReviews: 12,
-        countInStock: 30
+  {
+    name: "Classic Bundle",
+    description: "Classic breakfast, lunch and dinner for 7 days.",
+    image: "/images/classic.jpg",
+    price: 150,
+    rating: 4.5,
+    numReviews: 12,
+    countInStock: 50,
+  },
+  {
+    name: "Classic Plus Bundle",
+    description:
+      "Classic breakfast, lunch and dinner for 7 days, with a range of snacks. ",
+    image: "/images/classicplus.jpg",
+    price: 175,
+    rating: 4,
+    numReviews: 12,
+    countInStock: 40,
+  },
+  {
+    name: "Classic Deluxe Bundle",
+    description:
+      "Deluxe breakfast, lunch and dinner for 7 days. plus snacks, deserts andspecality drinks",
+    image: "/images/classicdeluxe.jpg",
+    price: 250,
+    rating: 4,
+    numReviews: 12,
+    countInStock: 35,
+  },
+  {
+    name: "Gluten-Free Bundle",
+    description: "Gluten-free breakfast, lunch and dinner for 7 days.",
+    image: "/images/gluten-free.jpg",
+    price: 150,
+    rating: 4.5,
+    numReviews: 12,
+    countInStock: 50,
+  },
+  {
+    name: "Gluten-Free Plus Bundle",
+    description:
+      "Gluten-free breakfast, lunch and dinner for 7 days, with a range of gluten-free snacks. ",
+    image: "/images/gfd.jpg",
+    price: 175,
+    rating: 4,
+    numReviews: 12,
+    countInStock: 40,
+  },
+  {
+    name: "Gluten-Free Deluxe Bundle",
+    description:
+      "Deluxe gluten-free breakfast, lunch and dinner for 7 days. plus snacks, deserts andspecality drinks",
+    image: "/images/glut-free-del.jpg",
+    price: 250,
+    rating: 4,
+    numReviews: 12,
+    countInStock: 35,
+  },
+  {
+    name: "Vegan Bundle",
+    description: " Vegan breakfast, lunch and dinner for 7 days. ",
+    image: "/images/vegan.jpg",
+    price: 150,
+    rating: 4.5,
+    numReviews: 12,
+    countInStock: 30,
+  },
+  {
+    name: "Vegan Plus Bundle",
+    description:
+      "Vegan breakfast, lunch and dinner for 7 days, with the added bonus of tasty snacks. ",
+    image: "/images/veganplus.jpg",
+    price: 175,
+    rating: 4,
+    numReviews: 8,
+    countInStock: 25,
+  },
 
-      }, 
-      {
-        name: "Vegan Plus Bundle", 
-        description: "Vegan breakfast, lunch and dinner for 7 days, with the added bonus of tasty snacks. ",
-        image: '/images/veganplus.jpg',
-        price: 175,
-        rating: 4,
-        numReviews: 8,
-        countInStock: 25
-      },
-      {
-        name: "Vegan Deluxe Bundle", 
-        description: "Deluxe vegan breakfast, lunch and dinner for 7 days, with extra snacks, deserts and drinks.",
-        image: '/images/vegandeluxe.jpg',
-        price: 250,
-        rating: 4,
-        numReviews: 8,
-        countInStock: 15
-      },
-      {
-        name: "Vegetarian Bundle", 
-        description: "Vegetarian breakfast, lunch and dinner for 7 days.",
-        image: '/images/vegetarian.jpg',
-        price: 150,
-        rating: 4,
-        numReviews: 12,
-        countInStock: 32 
-      },
-      {
-        name: "Vegetarian Plus Bundle", 
-        description: "Vegetarian breakfast, lunch and dinner for 7 days, with a range of snacks to keep you occupied. ",
-        image: '/images/vegetarianplus.jpg',
-        price: 175,
-        rating: 4,
-        numReviews: 12,
-        countInStock: 25
-      },
-      {
-        name: "Vegetarian Deluxe Bundle", 
-        description: "Deluxe vegetarian breakfast, lunch and dinner for 7 days, with extra snacks, deserts and specality drinks",
-        image: '/images/vegetariandeluxe.jpg',
-        price: 250,
-        rating: 4.5,
-        numReviews: 12,
-        countInStock: 50 
-      },
-      {
-        name: "Classic Bundle", 
-        description: "Classic breakfast, lunch and dinner for 7 days.",
-        image: '/images/classic.jpg',
-        price: 150,
-        rating: 4.5,
-        numReviews: 12,
-        countInStock: 50 
-      },
-      {
-        name: "Classic Plus Bundle", 
-        description: "Classic breakfast, lunch and dinner for 7 days, with a range of snacks. ",
-        image: '/images/classicplus.jpg',
-        price: 175,
-        rating: 4,
-        numReviews: 12,
-        countInStock: 40 
-      },
-      {
-        name: "Classic Deluxe Bundle", 
-        description: "Deluxe breakfast, lunch and dinner for 7 days. plus snacks, deserts andspecality drinks",
-        image: '/images/classicdeluxe.jpg',
-        price: 250,
-        rating: 4,
-        numReviews: 12,
-        countInStock: 35 
-      }
-]
+  {
+    name: "Vegan Deluxe Bundle",
+    description:
+      "Deluxe vegan breakfast, lunch and dinner for 7 days, with extra snacks, deserts and drinks.",
+    image: "/images/vegandeluxe.jpg",
+    price: 250,
+    rating: 4,
+    numReviews: 8,
+    countInStock: 15,
+  },
+  {
+    name: "Paleo Bundle",
+    description: "Pale breakfast, lunch and dinner for 7 days.",
+    image: "/images/paleo.jpg",
+    price: 150,
+    rating: 4.5,
+    numReviews: 12,
+    countInStock: 50,
+  },
+  {
+    name: "Paleo Plus Bundle",
+    description:
+      "Paleo breakfast, lunch and dinner for 7 days, with a range of gluten-free snacks. ",
+    image: "/images/paleo-plus.jpg",
+    price: 175,
+    rating: 4,
+    numReviews: 12,
+    countInStock: 40,
+  },
+  {
+    name: "Paleo Deluxe Bundle",
+    description:
+      "Deluxe paleo breakfast, lunch and dinner for 7 days. plus snacks, deserts andspecality drinks",
+    image: "/images/paleo-del.jpg",
+    price: 250,
+    rating: 4,
+    numReviews: 12,
+    countInStock: 35,
+  },
+  {
+    name: "Vegetarian Bundle",
+    description: "Vegetarian breakfast, lunch and dinner for 7 days.",
+    image: "/images/vegetarian.jpg",
+    price: 150,
+    rating: 4,
+    numReviews: 12,
+    countInStock: 32,
+  },
+  {
+    name: "Vegetarian Plus Bundle",
+    description:
+      "Vegetarian breakfast, lunch and dinner for 7 days, with a range of snacks to keep you occupied. ",
+    image: "/images/vegetarianplus.jpg",
+    price: 175,
+    rating: 4,
+    numReviews: 12,
+    countInStock: 25,
+  },
+  {
+    name: "Vegetarian Deluxe Bundle",
+    description:
+      "Deluxe vegetarian breakfast, lunch and dinner for 7 days, with extra snacks, deserts and specality drinks",
+    image: "/images/vegetariandeluxe.jpg",
+    price: 250,
+    rating: 4.5,
+    numReviews: 12,
+    countInStock: 50,
+  },
+];
 
-module.exports = products
+module.exports = products;
