236318f5153f8ddf2ce91b5747125eb52ab8b7ae
mjhwest89 <mjhwest89@gmail.com>
2022-02-22 11:00:15 +1030

    color changed

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const path = require('path');
 require('dotenv').config({path: path.join(__dirname, '../.env' )});
-console.log(process.env.MONGODB_URI)
+
 
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/iso-meals',
